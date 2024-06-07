c0e4b2d65b72029b24d74b371d438f5abc9f7bea
Adrien Bustany <adrien@bustany.org>
2024-06-07 12:24:08 +0200

    Add Rust to flake

--- flake.nix
+++ flake.nix
@@ -4,18 +4,27 @@
   inputs = {
     nixpkgs.url      = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url  = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [];
+        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        rust-version = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         rev = if (self ? shortRev) then self.shortRev else "dev";
 
         pkgNativeBuildInputs = [
+          rust-version
           pkgs.pkg-config
         ];
         pkgBuildInputs = [
